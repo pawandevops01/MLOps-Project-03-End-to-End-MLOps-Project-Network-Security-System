dc837776e631d9b4f468e0118dbf0447a03ea7b0
Asad Hanif <asadhanif3188@gmail.com>
2024-11-28 17:12:30 +0500

    Logging logic is updated

--- networksecurity/logging/logger.py
+++ networksecurity/logging/logger.py
@@ -2,16 +2,27 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+# Create logs directory if it doesn't exist
+logs_path = os.path.join(os.getcwd(), "logs")
 os.makedirs(logs_path, exist_ok=True)
 
+# Define a single log file
+LOG_FILE = "network_security_logs.log"
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# -----------------------------------------------------
+#  Old code as per course
+# -----------------------------------------------------
+# LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+# os.makedirs(logs_path, exist_ok=True)
+# LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+# -----------------------------------------------------
 
 logging.basicConfig(
     filename = LOG_FILE_PATH,
-    format = "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format = "[ %(asctime)s ] - line_number: %(lineno)d - module: %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
 
