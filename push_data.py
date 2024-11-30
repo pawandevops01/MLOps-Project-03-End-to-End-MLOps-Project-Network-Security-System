6ab1ae19a280764a6583c11cf36ed7f39c662b9a
Asad Hanif <asadhanif3188@gmail.com>
2024-11-30 22:00:10 +0500

    push_data.py code completed

--- push_data.py
+++ push_data.py
@@ -25,13 +25,41 @@ class NetworkDataExtract():
         
     def cv_to_json_convertor(self, file_path):
         try:
-            df = pd.read_csv(file_path)
-            df = df.dropna()
-            df = df.drop_duplicates()
-            df = df.reset_index(drop=True)
-            df = df.to_dict(orient='records')
-            return df
+            data = pd.read_csv(file_path)
+            data.reset_index(drop=True, inplace=True)
+            records = list(json.loads(data.T.to_json()).values())
+            return records
         except Exception as e:
             error = NetworkSecurityException(e, sys)
             logging.error(error.error_message)
             raise error
+        
+    def insert_data_to_mongodb(self, records, database, collection):
+        try:
+            self.database = database
+            self.collection = collection
+            self.records = records
+
+            self.mongo_client = pymongo.MongoClient(os.getenv("MONGO_DB_URL"), tlsCAFile=ca)
+            self.database = self.mongo_client[self.database]
+            self.collection = self.database[self.collection]
+
+            self.collection.insert_many(self.records)
+            return len(self.records)
+
+        except Exception as e:
+            error = NetworkSecurityException(e, sys)
+            logging.error(error.error_message)
+            raise error
+        
+if __name__ == "__main__":
+    DATA_FILE_PATH = "Network_Data/phisingData.csv"
+    DATABASE = "asad_db"
+    COLLECTION = "netwrok_phising_data"
+
+    data_extract = NetworkDataExtract()
+    records = data_extract.cv_to_json_convertor(file_path = DATA_FILE_PATH)
+    print(records)
+    no_of_records = data_extract.insert_data_to_mongodb(records = records, database= DATABASE, collection = COLLECTION)
+    print(no_of_records)
+
