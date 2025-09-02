4468f0068c33115d39b6793e8cd7892613c5014a
pawandevops01 <pawan.devops01@gmail.com>
2025-09-02 16:05:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-The setup.py file is an essential part of packaging and 
+The setup.py python file is an essential part of packaging and 
 distributing Python projects. It is used by setuptools 
 (or distutils in older Python versions) to define the configuration 
 of your project, such as its metadata, dependencies, and other settings.
